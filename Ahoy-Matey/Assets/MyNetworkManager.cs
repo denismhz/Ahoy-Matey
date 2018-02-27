39f1d9259f65e8a7b2d2c443c88140f762d3f414
Denis Manherz <denis.manherz@gmx.de>
2018-02-27 14:06:47 +0100

    Add MyNetWorkManager

---
+++ Ahoy-Matey/Assets/MyNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MyStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + "Starting Host." );
+        StartHost();
+    }
+
+    override public void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + "Host started.");
+    }
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client start requested.");
+        
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+        base.OnClientConnect(conn);
+    }
+}
